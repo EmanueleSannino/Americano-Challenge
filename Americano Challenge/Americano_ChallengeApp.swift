75a77db01024ba711ce55e754afc499f49ab6595
EmanueleSannino <sanninomanu06@gmail.com>
2024-12-03 23:06:08 +0100

    Initial Commit

---
+++ Americano Challenge/Americano_ChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Americano_ChallengeApp.swift
+//  Americano Challenge
+//
+//  Created by Emanuele Sannino on 03/12/24.
+//
+
+import SwiftUI
+
+@main
+struct Americano_ChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Americano ChallengeTests/Americano_ChallengeTests.swift
@@ -0,0 +1,17 @@
+//
+//  Americano_ChallengeTests.swift
+//  Americano ChallengeTests
+//
+//  Created by Emanuele Sannino on 03/12/24.
+//
+
+import Testing
+@testable import Americano_Challenge
+
+struct Americano_ChallengeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
